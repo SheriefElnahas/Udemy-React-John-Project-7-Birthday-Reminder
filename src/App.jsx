194840a1aba689231e652bb58a83c6ac12191029
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-10-04 06:26:14 +0200

    Basic Structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import data from './data';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello, World</h1>
+    </div>
+  )
+}
+
+export default App
